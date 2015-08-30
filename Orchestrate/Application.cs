5174542f4dba3c34748b7041cb54a7df427f2d06
jnewkirk <newkirk.jim@gmail.com>
2015-08-30 11:10:08 -0700

    Added Doc comments for IApplication and Application classes.

--- Orchestrate/Application.cs
+++ Orchestrate/Application.cs
@@ -2,22 +2,40 @@
 
 namespace Orchestrate.Io
 {
+    /// <summary>
+    /// This is a utility class provided as an implementation of the 
+    /// <see cref="IApplication"/> interface that looks for the Orchestrate 
+    /// Application Key in a specified environment variable. It also provides 
+    /// a default URL for the Orchestrate API. 
+    /// </summary>
     public class Application : IApplication
     {
-        string applicationKey;
+        string envVariable;
         string host;  
 
-        public Application(string applicationKey, string host = "https://api.orchestrate.io/")
+        /// <summary>
+        /// This class is constructed with an environment variable and  thedefault API URL. 
+        /// You can optionally provide a differenet URL which allows access to Orchestrate in 
+        /// mutiple data centers. 
+        /// </summary>
+        /// <param name="emvironmentVariable">The environment variable that holds the Orchestrate Application Key</param>
+        /// <param name="host">The API Host URL</param>
+        public Application(string envVariable, string host = "https://api.orchestrate.io/")
         {
-            this.applicationKey = applicationKey;
+            this.envVariable = envVariable;
             this.host = host;
         }
 
+        /// <summary>
+        /// The Key will lookup the passed in named environment variable for the 
+        /// Orchestrate API key. If the environment variable is not present 
+        /// it will return null. 
+        /// </summary>
         public string Key
         {
             get
             {
-                string result = Environment.GetEnvironmentVariable(applicationKey);
+                string result = Environment.GetEnvironmentVariable(envVariable);
                 if (result == null)
                     return string.Empty;
 
@@ -25,6 +43,7 @@ namespace Orchestrate.Io
             }
         }
 
+        /// <inheritdoc/>
         public string HostUrl
         {
             get { return host + "v0/"; }
--- Orchestrate/IApplication.cs
+++ Orchestrate/IApplication.cs
@@ -1,8 +1,21 @@
 ﻿namespace Orchestrate.Io
 {
+    /// <summary>
+    /// The IApplication interface is used by the Client class to provide access to
+    /// API Key and the URL of the Orchestrate Service. 
+    /// </summary>
     public interface IApplication
     {
+        /// <summary>
+        /// The string representing the API Key. You get this when you define an
+        /// application on the Orchestrate website. 
+        /// </summary>
         string Key { get; }
+        
+        /// <summary>
+        /// The URL that will be used to communicate from your application to the 
+        /// Orchestrate service. 
+        /// </summary>
         string HostUrl { get; }
     }
 }
